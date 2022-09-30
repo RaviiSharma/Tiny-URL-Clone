945c357124c2e5bc9916501d6a798f658baf0ea4
RaviiSharma <engineerravi036@gmail.com>
2022-09-30 12:35:32 +0530

    phase -1 completed

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,3 +1,4 @@
+
 const UrlModel = require("../models/urlModel")
 const ShortId = require("shortid")
 
@@ -19,7 +20,7 @@ const isValidUrl = function(value) {
 
     return regexForUrl.test(value);
 };
-//================================ createUrl =================================================//
+//1. API  ================================ createUrl ===========================================================================//
 
 const urlShortner = async function(req, res) {
     try{
@@ -45,7 +46,7 @@ const urlShortner = async function(req, res) {
     }
     const duplicateUrl = await UrlModel.findOne({ longUrl: longUrl }).select({ longUrl: 1, shortUrl: 1, urlCode: 1, _id: 0 })
     if (duplicateUrl) {
-        return res.status(409).send({ status: true,msg:"longUrl already exists", data: duplicateUrl }) //check the status code later
+        return res.status(200).send({ status: true, msg:"url shorten successfully", data: duplicateUrl }) //check the status code later
     }
 
         const urlCode = ShortId.generate().toLowerCase();
@@ -64,4 +65,60 @@ const urlShortner = async function(req, res) {
         res.status(500).send({ error: err.message });
     }
 }
-module.exports = { urlShortner}
+
+
+
+//2. API ========================================== GET URL ===========================================================================
+
+const getUrl = async function (req, res) {
+
+    const requestBody = req.body;
+    const queryParams = req.query;
+
+    try {
+
+        // query params must be empty
+        if (isValidRequest(queryParams)) {
+            return res
+                .status(400)
+                .send({ status: false, message: "invalid request" });
+        }
+
+        if (isValidRequest(requestBody)) {
+            return res
+                .status(400)
+                .send({ status: false, message: "input data is not required" });
+        }
+
+        const urlCode = req.params.urlCode;
+
+        // if (!/^(?=.[a-zA-Z].)[a-zA-Z\d!@#-_$%&*]{9,}$/.test(urlCode)) {
+        //     return res
+        //         .status(400)
+        //         .send({ status: false, message: " enter a valid urlCode" });
+        // }
+
+        const urlData = await UrlModel.findOne({ urlCode });
+
+        if (!urlData) {
+            return res
+                .status(404)
+                .send({ status: false, message: "no such url exist" });
+        }
+
+        if (urlData){
+            return res
+                .status(302)
+                .redirect(urlData.longUrl);
+        }
+
+    } catch (error) {
+
+        res
+            .status(500)
+            .send({ error: error.message });
+
+    }
+};
+
+module.exports = { urlShortner, getUrl}
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,12 +1,20 @@
 const express = require('express')
-const router = express.Router()
 const UrlController = require('../controllers/urlController')
 
+const router = express.Router()
 
 
 // API for url shortening 
 router.post('/url/shorten', UrlController.urlShortner )
 
+router.get("/:urlCode", UrlController.getUrl)
+
+
+router.all("/**", function (req,res){
+    res
+        .status(404)
+        .send({status:false, message:"the api you requested is not available"});
+})
 
 
 
